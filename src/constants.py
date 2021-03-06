c1851a57b6e8cec43bf73c6d259fb944a970ef3a
Christian Werner <chr.werner@me.com>
2021-03-06 14:42:54 +0000

    More fixes of new constants setup

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from src.checkpoint import CHECKPOINT
 
 # local directories
 PACKAGE_DIR = Path(__file__).parent.absolute()
 
-HOME_HTML="dummy"
-MODEL_CHECKPOINT_PATH="dummy"
+HOME_HTML = PACKAGE_DIR / "api" / "index.html"
+
+MODEL_CHECKPOINT_PATH = MODELS_DIR / CHECKPOINT
 
