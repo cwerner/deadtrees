1fc54cbfbc7ddf2b0c0c1a980b5790cf0e724a31
Christian Werner <christian.werner@kit.edu>
2021-05-31 13:56:09 +0200

    Make model checkpoint user-selectable in inference stage

--- merge_prediction_tiles.sh
+++
@@ -1,2 +0,0 @@
-gdal_merge.py -co 'COMPRESS=LZW' -co 'TILED=YES' -o test_mosaic2.tif data/predicted.2019/ortho_2019_ESPG3044_*
-
--- process_all_gpu.sh
+++
@@ -1,6 +0,0 @@
-for f in data/processed.images.2019/ortho_2019*
-do
-
-	stdbuf -i0 -o0 -e0 python deadtrees/deployment/tiler.py -o data/predicted.2019 $f
-done
-
