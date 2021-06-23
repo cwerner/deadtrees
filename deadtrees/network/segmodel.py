13c34a3946526d91cb527a45dcf972f55b033f83
Christian Werner <chr.werner@me.com>
2021-06-23 13:04:36 +0000

    Remove tversky and ce loss code and switch to diceceloss from monai

--- deadtrees/network/segmodel.py
+++ deadtrees/network/segmodel.py
@@ -2,40 +2,15 @@
 
 import logging
 
+import monai
+
 import pytorch_lightning as pl
 import torch
-from deadtrees.loss.tversky.binary import BinaryTverskyLossV2
 from deadtrees.visualization.helper import show
 from omegaconf import DictConfig
 from pl_bolts.models.vision.unet import UNet
-from torch.nn import functional as F
 
 logger = logging.getLogger(__name__)
-
-
-# Taken from https://github.com/justusschock/dl-utils/blob/master/dlutils/metrics/dice.py
-def binary_dice_coefficient(
-    pred: torch.Tensor, gt: torch.Tensor, thresh: float = 0.5, smooth: float = 1e-7
-) -> torch.Tensor:
-    """
-    computes the dice coefficient for a binary segmentation task
-
-    Args:
-        pred: predicted segmentation (of shape Nx(Dx)HxW)
-        gt: target segmentation (of shape NxCx(Dx)HxW)
-        thresh: segmentation threshold
-        smooth: smoothing value to avoid division by zero
-
-    Returns:
-        torch.Tensor: dice score
-    """
-
-    assert pred.shape == gt.shape
-
-    pred_bool = pred > thresh
-
-    intersec = (pred_bool * gt).float()
-    return 2 * intersec.sum() / (pred_bool.float().sum() + gt.float().sum() + smooth)
 
 
 class SemSegment(UNet, pl.LightningModule):  # type: ignore
@@ -47,11 +22,7 @@ class SemSegment(UNet, pl.LightningModule):  # type: ignore
         super().__init__(**network_conf)
         self.save_hyperparameters()  # type: ignore
 
-        self.binary_tversky_loss = BinaryTverskyLossV2(
-            alpha=1.0 - train_conf["tversky_beta"],
-            beta=train_conf["tversky_beta"],
-        )
-        self.ce_loss = torch.nn.CrossEntropyLoss()
+        self.criterion = monai.losses.DiceCELoss(softmax=True)
 
     def get_progress_bar_dict(self):
         """Hack to remove v_num from progressbar"""
@@ -65,50 +36,27 @@ class SemSegment(UNet, pl.LightningModule):  # type: ignore
         img = img.float()
         mask = mask.long()
         pred = self(img)
-        softmaxed_pred = torch.nn.functional.softmax(pred, dim=1)
-        a_max = torch.argmax(softmaxed_pred, dim=1)
 
-        # Calculate losses
-        ce_loss = self.ce_loss(pred, mask)
-        binary_tversky_loss = self.binary_tversky_loss(a_max, mask.unsqueeze(dim=1))
-        total_loss = (ce_loss + binary_tversky_loss) / 2
+        # TODO: simplify
+        mask2 = torch.zeros_like(pred).scatter_(1, mask.unsqueeze(1), 1)
 
-        # Calculate dice coefficient
-        dice_coeff = binary_dice_coefficient(a_max, mask)
-        accuracy = (a_max == mask).float().mean()
+        loss = self.criterion(pred, mask2)
 
-        self.log("train/dice_coeff", dice_coeff, prog_bar=True)
-        self.log("train/accuracy", accuracy, prog_bar=True)
+        self.log("train/total_loss", loss)
 
-        self.log("train/ce_loss", ce_loss)
-        self.log("train/binary_tversky_loss", binary_tversky_loss)
-        self.log("train/total_loss", total_loss)
-
-        return total_loss
+        return loss
 
     def validation_step(self, batch, batch_idx):
         img, mask, stats = batch
         img = img.float()
         mask = mask.long()
         pred = self(img)
-        softmaxed_pred = torch.nn.functional.softmax(pred, dim=1)
-        a_max = torch.argmax(softmaxed_pred, dim=1)
 
-        # Calculate losses
-        ce_loss = self.ce_loss(pred, mask)
-        binary_tversky_loss = self.binary_tversky_loss(a_max, mask.unsqueeze(dim=1))
-        total_loss = (ce_loss + binary_tversky_loss) / 2
+        mask2 = torch.zeros_like(pred).scatter_(1, mask.unsqueeze(1), 1)
 
-        # Calculate dice coefficient
-        dice_coeff = binary_dice_coefficient(a_max, mask)
-        accuracy = (a_max == mask).float().mean()
+        loss = self.criterion(pred, mask2)
 
-        self.log("val/dice_coeff", dice_coeff)
-        self.log("val/accuracy", accuracy, prog_bar=True)
-
-        self.log("val/ce_loss", ce_loss)
-        self.log("val/binary_tversky_loss", binary_tversky_loss)
-        self.log("val/total_loss", total_loss)
+        self.log("val/total_loss", loss)
 
         if batch_idx == 0:
             sample_chart = show(
@@ -134,30 +82,19 @@ class SemSegment(UNet, pl.LightningModule):  # type: ignore
                         commit=False,
                     )
 
-        return total_loss
+        return loss
 
     def test_step(self, batch, batch_idx):
         img, mask, stats = batch
         img = img.float()
         mask = mask.long()
         pred = self(img)
-        softmaxed_pred = torch.nn.functional.softmax(pred, dim=1)
-        a_max = torch.argmax(softmaxed_pred, dim=1)
 
-        # Calculate losses
-        ce_loss = self.ce_loss(pred, mask)
-        binary_tversky_loss = self.binary_tversky_loss(a_max, mask.unsqueeze(dim=1))
-        total_loss = (ce_loss + binary_tversky_loss) / 2
+        mask2 = torch.zeros_like(pred).scatter_(1, mask.unsqueeze(1), 1)
 
-        # Calculate dice coefficient
-        dice_coeff = binary_dice_coefficient(a_max, mask)
-        accuracy = (a_max == mask).float().mean()
+        loss = self.criterion(pred, mask2)
 
-        self.log("test/dice_coeff", dice_coeff)
-        self.log("test/accuracy", accuracy)
-        self.log("test/ce_loss", ce_loss)
-        self.log("test/binary_tversky_loss", binary_tversky_loss)
-        self.log("test/total_loss", total_loss)
+        self.log("test/total_loss", loss)
 
     def configure_optimizers(self):
         opt = torch.optim.Adam(
