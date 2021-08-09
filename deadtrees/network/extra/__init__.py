907ffad2b43a94f00d0b5211791650dc401d48bf
Christian Werner <christian.werner@kit.edu>
2021-08-09 13:48:48 +0200

    Add copy of external models that current held up in a PR - will be removed once PR is merged

---
+++ deadtrees/network/extra/__init__.py
@@ -0,0 +1,3 @@
+# models copied from https://github.com/jlcsilva/segmentation_models.pytorch
+# remove once PR is merged into the original repo: https://github.com/qubvel/segmentation_models.pytorch/pull/425
+# date: 2021-08-09
