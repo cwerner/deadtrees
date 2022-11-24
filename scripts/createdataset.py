a8198d09370a960bfe893d91ec79e443c01b3ef0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 00:14:07 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/createdataset.py
+++ scripts/createdataset.py
@@ -340,7 +340,29 @@ def main():
         # untar input
         for tf_name in sorted((args.outdir / args.sub_dir).glob("train-00*.tar")):
             with tarfile.open(tf_name) as tf:
-                tf.extractall(tmpdir)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, tmpdir)
 
         print("Write balanced shards from deadtree samples")
         df = pd.read_csv(args.outdir / args.stats_file)
